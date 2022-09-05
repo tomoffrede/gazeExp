045ae497f8b0aed5592ee1f21b291de582e58335
Thomaz Freire Offrede <offredet@german.hu-berlin.de>
2022-09-05 17:12:44 +0200

    .

--- PlotF0.R
+++ PlotF0.R
@@ -7,7 +7,12 @@ library(tidyverse)
 folder <- "C:/Users/offredet/Documents/1HU/ExperimentEyes/Data/All/"
 folderPlots <- "C:/Users/offredet/Documents/1HU/ExperimentEyes/Data/f0Plots/"
 
+
+# folder <- "C:/Users/offredet/Documents/1HU/ExperimentEyes/Data/PerSpeaker/QQE/"
+# folderPlots <- "C:/Users/offredet/Documents/1HU/ExperimentEyes/f0par/"
+
 files <- list.files(folder, "\\.txt")
+files <- files[!grepl("Register", files)]
 
 for(i in files){
   f <- read.table(paste0(folder, i), header=TRUE, na.strings = "--undefined--")
