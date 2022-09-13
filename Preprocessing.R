847ebc88f29a2f15e04292c479becd801d7bdcff
Thomaz Freire Offrede <offredet@german.hu-berlin.de>
2022-09-13 18:00:59 +0200

    fixing things

--- Preprocessing.R
+++ Preprocessing.R
@@ -116,7 +116,7 @@ summary(lmer(gap ~ condition + (1 | file), tG))
 
 # ACOUSTICS
 
-## (taken from AudioData.R)
+## (adapted from AudioData.R)
 
 filesTG <- list.files(folder, "\\.TextGrid")
 filesTG <- filesTG[!grepl("VUV", filesTG)]
@@ -135,7 +135,6 @@ startTime <- Sys.time()
 for(i in 1:nrow(files)){
   tg <- tg.read(paste0(folder, files$filesTG[[i]]), encoding=detectEncoding(paste0(folder, files$filesTG[[i]])))
   txt <- read.table(paste0(folder, files$filesTXT[[i]]), header=TRUE, na.strings = "--undefined--")
-  turnCount <- 0
   
   if(substr(files$filesTG[i], 4, 5) == "BL"){ # baseline speech!
     ipuCount <- 0
@@ -151,7 +150,7 @@ for(i in 1:nrow(files)){
                 summarize(f = mean(f0mean, na.rm=TRUE)))[1,1] # "[1,1]" because `f` is a 1x1 matrix data frame
         f0[nrow(f0)+1,] <- c(substr(files$filesTG[i], 1, 9),
                              substr(files$filesTG[i], 7, 9),
-                             "baseline",
+                             "baseline", # "turn"
                              startBL,
                              endBL,
                              as.numeric(endBL - startBL), # duration of entire baseline
@@ -163,6 +162,7 @@ for(i in 1:nrow(files)){
       }
     }
   } else if(substr(files$filesTG[i], 4, 5) == "CO"){ # conversation
+    turnCount <- 0
     for(n in 1:tg.getNumberOfIntervals(tg, "participant")){
       if(tg.getLabel(tg, "participant", n) == "s"){
         ipuCount <- 0
@@ -174,20 +174,22 @@ for(i in 1:nrow(files)){
             ipuCount <- ipuCount + 1
             startIPU <- as.numeric(tg.getIntervalStartTime(tg, "IPU", p))
             endIPU <- as.numeric(tg.getIntervalEndTime(tg, "IPU", p))
-            f <- (txt %>%
-                    filter(onset >= startIPU & offset <= endIPU) %>%
-                    summarize(f = mean(f0mean, na.rm=TRUE)))[1,1] # "[1,1]" because `f` is a 1x1 matrix data frame
-            f0[nrow(f0)+1,] <- c(substr(files$filesTG[i], 1, 9),
-                                 substr(files$filesTG[i], 7, 9),
-                                 turnCount,
-                                 turnOnset,
-                                 turnOffset,
-                                 as.numeric(turnOffset - turnOnset), # duration of turn
-                                 ipuCount,
-                                 startIPU,
-                                 endIPU,
-                                 as.numeric(endIPU - startIPU), # duration of IPU
-                                 as.numeric(f))
+            if(txt$onset >= startIPU & txt$offset <= endIPU){
+              f <- (txt %>%
+                      filter(onset >= startIPU & offset <= endIPU) %>%
+                      summarize(f = mean(f0mean, na.rm=TRUE)))[1,1] # "[1,1]" because `f` is a 1x1 matrix data frame
+              f0[nrow(f0)+1,] <- c(substr(files$filesTG[i], 1, 9),
+                                   substr(files$filesTG[i], 7, 9),
+                                   turnCount,
+                                   turnOnset,
+                                   turnOffset,
+                                   as.numeric(turnOffset - turnOnset), # duration of turn
+                                   ipuCount,
+                                   startIPU,
+                                   endIPU,
+                                   as.numeric(endIPU - startIPU), # duration of IPU
+                                   as.numeric(f))
+            }
           }
         }
       }
@@ -203,20 +205,22 @@ for(i in 1:nrow(files)){
             ipuCount <- ipuCount + 1
             startIPU <- as.numeric(tg.getIntervalStartTime(tg, "IPU", p))
             endIPU <- as.numeric(tg.getIntervalEndTime(tg, "IPU", p))
-            f <- (txt %>%
-                    filter(onset >= startIPU & offset <= endIPU) %>%
-                    summarize(f = mean(f0mean, na.rm=TRUE)))[1,1] # "[1,1]" because `f` is a 1x1 matrix data frame
-            f0[nrow(f0)+1,] <- c(substr(files$filesTG[i], 1, 9),
-                                 paste0(substr(files$filesTG[i], 7, 9), "-Robot"),
-                                 turnCount,
-                                 turnOnset,
-                                 turnOffset,
-                                 as.numeric(turnOffset - turnOnset), # duration of turn
-                                 ipuCount,
-                                 startIPU,
-                                 endIPU,
-                                 as.numeric(endIPU - startIPU), # duration of IPU
-                                 as.numeric(f))
+            if(txt$onset >= startIPU & txt$offset <= endIPU){
+              f <- (txt %>%
+                      filter(onset >= startIPU & offset <= endIPU) %>%
+                      summarize(f = mean(f0mean, na.rm=TRUE)))[1,1] # "[1,1]" because `f` is a 1x1 matrix data frame
+              f0[nrow(f0)+1,] <- c(substr(files$filesTG[i], 1, 9),
+                                   paste0(substr(files$filesTG[i], 7, 9), "-Robot"),
+                                   turnCount,
+                                   turnOnset,
+                                   turnOffset,
+                                   as.numeric(turnOffset - turnOnset), # duration of turn
+                                   ipuCount,
+                                   startIPU,
+                                   endIPU,
+                                   as.numeric(endIPU - startIPU), # duration of IPU
+                                   as.numeric(f))
+            }
           }
         }
       }
@@ -245,70 +249,144 @@ save(f0, file=paste0(folder, "f0.RData"))
 
 load(paste0(folder, "f0.RData"))
 
+####check
+
+ch <- f0 %>% filter(speaker=="AMO", condition=="NG", task=="Conversation")
+a <- ch %>% filter(turn==1)
+b <- ch %>% filter(turn==2)
+c <- ch %>% filter(turn==3)
+d <- ch %>% filter(turn==4)
+
+#### end of check
+
 f0 <- f0 %>% 
   mutate(participant = substr(file, 7, 9), # variable with name of speaker including for the robot subsets
          groupings = paste(speaker, condition, task, sep = "."))
 
-for(i in unique(f0$participant)){
-  dat0 <- f0 %>% filter(participant == i)
-  par(mfrow=c(3, 2))
-  for(g in unique(dat0$groupings)){
-    dat <- dat0 %>% filter(groupings == g)
-    plot(dat$IPU, dat$f0mean, type="l", main=g)
-  }
-  readline("Enter to continue")
-}
+# for(i in unique(f0$participant)){
+#   dat0 <- f0 %>% filter(participant == i)
+#   par(mfrow=c(3, 2))
+#   for(g in unique(dat0$groupings)){
+#     dat <- dat0 %>% filter(groupings == g)
+#     plot(dat$IPU, dat$f0mean, type="l", main=g)
+#   }
+#   readline("Enter to continue")
+# }
 
 # lots of weird data!
 
 # calculate proportion of NA
-# 
-# f0 <- f0 %>% 
-#   group_by(speaker, condition, task, turn) %>% 
-#   mutate(NAprop = sum(is.na(f0mean)) / n()) %>% 
-#   ungroup()
-# 
-# t <- f0 %>% 
-#   mutate(tgroup = paste(speaker, condition, task, turn, collapse = "."))
-# length(unique(t$tgroup))
+
+f0 <- f0 %>%
+  # group_by(speaker, condition, task) %>%
+  group_by(groupings) %>%
+  mutate(NAprop = sum(is.na(f0mean)) / n()) %>%
+  ungroup()
+
 # length(unique(f0$NAprop))
+# length(unique(f0$groupings))
 
-c <- data.frame(matrix(nrow=0, ncol=4))
-names(c) <- c("turn", "f0mean", "groupings", "NAprop")
+# in the following i think the idea was to get the amount of NA also grouping per turn of each speaker
+# but i don't see the point in that anymore, so forget it.
+# 
+# c <- data.frame(matrix(nrow=0, ncol=4))
+# names(c) <- c("turn", "f0mean", "groupings", "NAprop")
+# 
+# for(g in unique(f0$groupings)){
+#   c0 <- f0 %>% 
+#     filter(groupings == g) %>% 
+#     select(turn, f0mean, groupings) %>%
+#     mutate(NAprop = sum(is.na(f0mean)) / n())
+#   c <- rbind(c, c0)
+# }
+# 
+# f0a <- full_join(f0, c, by=c("groupings", "turn", "f0mean"), all=TRUE)
+# f0a <- f0a %>% distinct()
+# # na <- f0a[!duplicated(f0a$groupings)]
+# na <- f0a %>% distinct(groupings, .keep_all=TRUE)
+# hist(na$NAprop)
 
-for(g in unique(f0$groupings)){
-  c0 <- f0 %>% 
-    filter(groupings == g) %>% 
-    select(turn, f0mean, groupings) %>%
-    mutate(NAprop = sum(is.na(f0mean)) / n())
-  c <- rbind(c, c0)
-}
+# end of unnecessarily complicated code
 
-f0a <- full_join(f0, c, by=c("groupings", "turn", "f0mean"), all=TRUE)
-f0a <- f0a %>% distinct()
-# na <- f0a[!duplicated(f0a$groupings)]
-na <- f0a %>% distinct(groupings, .keep_all=TRUE)
-hist(na$NAprop)
+# hist(f0$NAprop)
+
+# I'm not sure how to deal with it. why is there so much NA?
+# should we exclude files with too many NAs? what would be the cutoff point?
 
 # exclude f0 higher than 2.5 SD
 
 n <- data.frame(matrix(nrow=0, ncol=4))
 names(n) <- c("turn", "f0mean", "groupings", "f0z")
 
-for(g in unique(f0a$groupings)){
-  n0 <- f0a %>% 
+for(g in unique(f0$groupings)){
+  n0 <- f0 %>% 
     filter(groupings == g) %>% 
     select(turn, f0mean, groupings) %>%
     mutate(f0z = (f0mean - mean(f0mean, na.rm=TRUE))/sd(f0mean, na.rm=TRUE))
   n <- rbind(n, n0)
 }
 
-f0b <- full_join(f0a, n, by=c("groupings", "turn", "f0mean"), all=TRUE)
+f0b <- full_join(f0, n, by=c("groupings", "turn", "f0mean"), all=TRUE)
 f0b <- f0b %>% distinct()
 f0b <- f0b %>% filter(abs(f0z) < 2.5)
 
 # from here: create new variable with the f0 of partner's previous turn (f0 mean I guess)
-# do same for MFCCs
-# then do UBB-UGM (???) models with MFCCs
 
+dat <- f0b
+
+# calculate mean f0 per turn
+# (mean f0 of entire baseline for BL files)
+# save that as the f0 of the interlocutor's following turn
+
+dat$tgroup <- paste(dat$groupings, dat$turn, sep=".")
+
+# dt <- dat %>% # trying to get the mean per turn (per speaker per task per condition), but this code keeps grouping the dataset by file, not by `tgroup`
+#   group_by(tgroup) %>% 
+#   summarize(f0turn = mean(f0mean, na.rm=TRUE)) %>% # i've also tried with mutate()
+#   ungroup() #%>% 
+  # select(-c("f0mean", "f0z", "IPU", "IPUOnset", "IPUOffset", "IPUDur")) %>% 
+  # distinct()
+
+dt <- data.frame(matrix(nrow=0, ncol=2))
+names(dt) <- c("tgroup", "f0turn")
+
+t=unique(dat$tgroup[!grepl("baseline", dat$tgroup)])[[5]]
+
+for(t in unique(dat$tgroup)){
+  d <- dat %>% 
+    filter(tgroup == t)
+  dt[nrow(dt)+1,] <- c(t, mean(d$f0mean, na.rm=TRUE))
+}
+
+dat <- dat %>% 
+  select(-c("f0mean", "f0z", "IPU", "IPUOnset", "IPUOffset", "IPUDur")) %>% 
+  distinct()
+ 
+dat <- merge(dat, dt, by="tgroup")
+
+# why does even this group it by file and not tgroup??????
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+ch <- dat[,c(1:3, 18,19)]
+
+dat$prevf0 <- NA
+
+for(i in 2:nrow(dat)){
+  dat$prevf0[i] <- NA
+}
 
