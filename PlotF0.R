8fa272e909dc2ffe181f695815f64f20d9bdf45e
Thomaz Freire Offrede <offredet@german.hu-berlin.de>
2022-12-06 19:24:42 +0100

    now you can also create plots with the F0 from the dataset (before: only from the TXT files created by Praat)

--- PlotF0.R
+++ PlotF0.R
@@ -7,6 +7,7 @@ library(tidyverse)
 folder <- "C:/Users/offredet/Documents/1HU/ExperimentEyes/Data/All/"
 folderPlots <- "C:/Users/offredet/Documents/1HU/ExperimentEyes/Data/f0Plots/"
 
+# F0 extracted with Praat
 
 # folder <- "C:/Users/offredet/Documents/1HU/ExperimentEyes/Data/PerSpeaker/QQE/"
 # folderPlots <- "C:/Users/offredet/Documents/1HU/ExperimentEyes/f0par/"
@@ -21,3 +22,27 @@ for(i in files){
   plot(f$f0mean)
   dev.off()
 }
+
+# F0 averaged over IPUs
+
+load(paste0(folder, "DataConversation.RData"))
+
+for(s in unique(dac$speaker)){
+  d <- dac %>% filter(speaker == s, !is.na(f0mean))
+  
+  png(paste0(folderPlots, s, ".png"), width=750, height=750)
+  plot(d$turn, d$f0mean, main = paste0(s, " - data points: ", nrow(d)), cex=3)
+  dev.off()
+}
+
+
+
+
+
+
+
+
+
+
+
+
